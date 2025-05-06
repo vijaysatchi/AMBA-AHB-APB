9227546f26091ab96b4728ec4e66e19e68062b50
Vijay Satchi <101689049+vijaysatchi@users.noreply.github.com>
2025-05-06 16:28:52 -0400

    fixed how prdata is outputted, made penable and psel deassert earlier

--- apb_master.v
+++ apb_master.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 100ps
+`timescale 1ns / 1ns
 module apb_master(
     input PCLK,
     input PRESETn, //low active
@@ -19,28 +19,31 @@ module apb_master(
 );
     reg [1:0] state; //00 = idle, 01 = setup, 02 = access    
     
+
+	/*
     initial begin
         state <= 2'b00;
     end
 
-    /*
+    
 	might be worse to have this in another process 
 	but who knows at least it look kinda cleaner maybe kinda
 	*/
-  always @ (posedge PCLK) begin
-     if(state == 2'b01) begin
-	if (!READ_WRITE) begin 
-	//READ
-		PADDR <= PADDR_IN;
-		PWRITE <= 1'b0;
-	end else
-	//WRITE
-		PADDR <= PADDR_IN;
-		PWDATA <= PWDATA_IN;
-		PWRITE <= 1'b1;
-		
-        end
-    end
+	always @ (posedge PCLK) begin
+    	if(state == 2'b01) begin
+			if (!READ_WRITE) begin 
+				//READ
+				PADDR <= PADDR_IN;
+				PWRITE <= 1'b0;
+			end else begin
+				//WRITE
+				PADDR <= PADDR_IN;
+				PWDATA <= PWDATA_IN;
+				PWRITE <= 1'b1;	
+	        end
+	    end
+	end
+	
     
     always @(posedge PCLK) begin 
         if (PRESETn == 1'b1) begin
@@ -57,19 +60,31 @@ module apb_master(
                 2'b01: begin //SETUP
                     PSEL <= 1'b1;
                     PENABLE <= 1'b0;
-		    state <= 2'b10;            
+		    		state <= 2'b10;            
                 end
                 2'b10: begin //ACCESS
                     PSEL <= 1'b1; //i changed this
                     PENABLE <= 1'b1; //i chnaged this too    
                     if (PREADY && !TRANSFER) begin
-			PADDR <= 0;
-			PWRITE <= 0;
-			PWDATA <= 0;
-			//PENABLE <= 1'b1;
+						PADDR <= 0;
+						PWRITE <= 0;
+						PWDATA <= 0;
+						PENABLE <= 1'b0;
+						PSEL <= 1'b0;
                         state <= 2'b00; // go idle
                     end else if (PREADY && TRANSFER) begin    
-                        state <= 2'b01; // go setup, begin assert pwrite, penable, paddr, pdata
+                        state <= 2'b01; // go setup, begin assert pwrite, penable, paddr, pdata						
+						/*
+						if (!READ_WRITE) begin 
+							//READ
+							PADDR <= PADDR_IN;
+							PWRITE <= 1'b0;
+						end else begin
+							//WRITE
+							PADDR <= PADDR_IN;
+							PWDATA <= PWDATA_IN;
+							PWRITE <= 1'b1;	
+				        end */
                     end else if (!PREADY) begin    
                         state <= 2'b10; //go access (loop access)
                     end
