7dd74c7d2758af26e537356483033174c825c1de
Vijay Satchi <101689049+vijaysatchi@users.noreply.github.com>
2025-05-06 16:29:48 -0400

    fixed how the presetn was triggered and also fixed transactions with the memory

--- apb_slave.v
+++ apb_slave.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 100ps
+`timescale 1ns / 1ns
 module apb_slave(
 	PSELx,
 	PENABLE,
@@ -17,23 +17,34 @@ input [31:0] PADDR, PWDATA;
 output reg PREADY, PSLVERR;
 output reg [31:0] PRDATA;
 
-reg [31:0] memory [31:0];
+reg [31:0] MEMORY [31:0]; 
 
 always @ (posedge PCLK) begin
-    if (PSELx && !PRESETn) begin
-	PREADY <= 1'b1;
-    end 
-end
-
-always @ (posedge PCLK) begin
-    if (PSELx && PENABLE && !PRESETn) begin
-	if (PWRITE) begin
-	    $display("W; ADDR=%d, DATA=%d", PADDR, PWDATA);
-	    PRDATA <= PWDATA;
-	end else begin
-	    $display("R; ADDR=%d, DATA=%d", PADDR, PRDATA);
-	end
+	if(PRESETn) begin
+		PREADY <= 1'b0;
+		PRDATA <= 32'b0;
+    end else begin 
+		if (PSELx && PENABLE) begin
+			if (!PWRITE) begin
+	    		PRDATA <= MEMORY[PADDR];
+				PREADY <= 1'b1;
+				$display("R; ADDR=%d, DATA=%d", PADDR, PRDATA);
+			end else begin
+				MEMORY[PADDR] <= PWDATA;
+				PREADY <= 1'b1;
+	   			$display("W; ADDR=%d, DATA=%d", PADDR, PWDATA);
+			end
+		end else begin
+			PREADY <= 1'b0;
+		end
     end
 end
+
+/*
+always @ (posedge PCLK) begin
+	if (PRESETn) begin
+		PREADY <= 1'b0;
+	end
+end*/
 
 endmodule
