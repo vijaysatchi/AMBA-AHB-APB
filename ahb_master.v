f9376b5e2b6cfe59a19f0ab50ac10807e7945945
Vijay Satchi <101689049+vijaysatchi@users.noreply.github.com>
2025-05-19 18:03:32 -0400

    Update ahb_master.v
    - changed structure from FSM to if's because it doesnt make sense to have FSM when its pipelined
    - HSEL is being set properly
    - still doesn't take into account HREADY == 0 for stalls
    - hsize, wrap hasn't been implemented
    - incr bursts and htrans work now

--- ahb_master.v
+++ ahb_master.v
@@ -1,11 +1,12 @@
+
 `timescale 1ns / 1ns
 module ahb_master(
     input HCLK,
     input HRESETn, //low active
     input [31:0] HRDATA,
     input [31:0] HWDATA_IN,
-	input [2:0] HBURST_IN,
-	input [2:0] HSIZE_IN,
+    input [2:0] HBURST_IN,
+    input [2:0] HSIZE_IN,
     input HREADY1,
     input HREADY2,
     input HREADY3,
@@ -31,60 +32,149 @@ module ahb_master(
     output reg HMASTLOCK
 
 );
-    reg [1:0] state, next_state = 2'b00; //00 = idle, 01 = address & control signals, 02 = data    
 
-always @(*) begin
-	if (HRESETn == 1'b1) begin
-		next_state = 2'b00;
-	end else begin
-	case(state) begin
-		2'b00: begin
-			if(TRANSFER) begin
-				next_state = 2'b01;
-			end
-		end
-		2'b01: begin //address phase
-			next_state = 2'b10; //address phase must always be 1 cycle
-		end
-		2'b10: begin
-			/*if(HREADY) begin 
-				next_state = 2'b00;
-			end*/
-		end
-	end
-end
+	// parameter [1:0] IDLE = 2'b00, ADDRESS = 2'b01, DATA = 2'b10;
+    // reg [1:0] state, next_state = IDLE; //00 = idle, 01 = address & control signals, 02 = data    
+    reg state;
 
+	reg [5:0] beat_count, burst_total;
+	reg [31:0] address_incr;
 
-always @(posedge HCLK) begin
-	/*	
-	if (HRESETn == 1'b1) begin
-		state <= 2'b00;
-	end else begin*/
-	state <= next_state;
-	case(state)
-		2'b00: begin
-			if(TRANSFER) begin
-				//state <= 2'b01;
-				//begin address phase assuming hready is not set
+	always @(posedge HCLK) begin
+		if(HRESETn) begin
+			//RESET EVERYTHING
+			HSIZE <= 3'b0;
+			HTRANS <= 2'b0;
+			HBURST <= 3'b0;
+			HPROT <= 4'b0;
+			HMASTLOCK <= 1'b0;
+			HADDR <= 32'b0;
+			HWRITE <= 1'b0;
+			HSEL1 <= 1'b0;
+			HSEL2 <= 1'b0;
+			HSEL3 <= 1'b0;
+			HSEL4 <= 1'b0;
+			HWDATA <= 32'b0;
+			burst_total <= 6'b0;
+			beat_count <= 6'b1;
+			state <= 2'b0;
+		end else begin
+			if(TRANSFER && state == 2'b00) begin
+				/*
+				explanation of this if block:
+				- when: happens when any transfer, single or burst first begins
+				- sets addr + ctrl signals then does state <= 1 to begin the data phase in the next cycle
+				- 
+				*/
+				state <= 2'b01;
+				//INITIAL ADDRESS + CONTROL PHASE
+				HSIZE <= HSIZE_IN;
+				HTRANS <= 2'b10;
+				HBURST <= HBURST_IN;
+				HPROT <= 4'b0; //idk this doesnt matter lmao
 				HADDR <= HADDR_IN;
 				HWRITE <= READ_WRITE;
-				HBURST <= HBURST_IN;
-				HTRANS <= 2'b10;
-				HSIZE <= HSIZE_IN;
+				HSEL1 <= 1; //temp until multiple slaves working
+				case (HBURST_IN)
+					3'b000: begin //SINGLE
+						burst_total = 1;
+					end
+					3'b010, //WRAP4
+					3'b011: begin //INCR4
+						burst_total = 4;
+					end	
+					3'b100, //WRAP8
+					3'b101: begin //INCR8
+						burst_total = 8;
+					end	
+					3'b110, //WRAP16
+					3'b111: begin //INCR16
+						burst_total = 16;
+					end	
+					default: begin //INCR
+						burst_total = 2; //putting 2 here temporarily maybe we need an extra input signal for this idk
+					end
+				endcase
 			end
-		end
-		2'b01: begin
-			state <= 2'b10;
-			if(READ_WRITE) begin
-				HWDATA <= HWDATA_IN;
-			end
-			if(TRANSFER) begin
-				HADDR <= HADDR_IN;
-				HWRITE <= READ_WRITE;
-				HBURST <= HBURST_IN;
-				HTRANS <= 2'b10;
-				HSIZE <= HSIZE_IN;
+			if(state == 2'b01) begin
+				/*
+				explanation of this if block:
+				- when: this is the data phase, but it also sets the addy + ctrl signals for the next transfer IF:
+					- if ( part of a burst OR a new transfer is to happen)
+				- 
+				*/
+				//DATA PHASE 
+				if(READ_WRITE) HWDATA <= HWDATA_IN;
+				if(beat_count < burst_total || TRANSFER) begin 
+					HSIZE <= HSIZE_IN;
+					HBURST <= HBURST_IN;
+					HPROT <= 4'b0; //idk this doesnt matter lmao
+					HADDR <= HADDR_IN;
+					HWRITE <= READ_WRITE;
+					HSEL1 <= 1;
+					if(TRANSFER) HTRANS <= 2'b10;
+					else begin //this happens if (beat_count < burst_total) == TRUE 
+						HTRANS <= 2'b11;
+						beat_count <= beat_count + 1;
+						HADDR <= HADDR + 4; //temp until hsize working
+						/*case (HBURST_IN)
+							3'b010, //WRAP4
+							3'b011: begin //INCR4
+								HADDR <= HADDR + 4;
+							end	
+							3'b100, //WRAP8
+							3'b101: begin //INCR8
+								HADDR <= HADDR + 8;
+							end	
+							3'b110, //WRAP16
+							3'b111: begin //INCR16
+								HADDR <= HADDR + 16;
+							end	
+							default: begin //INCR
+								HADDR <= HADDR + 2; //putting 2 here temporarily maybe we need an extra input signal for this idk
+							end
+						endcase*/
+					end
+				end else if (beat_count == burst_total) begin 
+					state <= 2'b00;
+					HSEL1 <= 0;
+					//HADDR <= 0;
+					beat_count <= 1;
+					HTRANS <= 0;
+				end
+				
 			end
 		end
 	end
-end
+endmodule
+
+//	always @(*) begin
+//		if(!HRESETN) begin
+//			next_state <= 2'b00;
+//		end else begin
+//			case(state)
+//				IDLE: begin
+//					if(TRANSFER) next_state <= ADDRESS;
+//				end
+//				ADDRESS: begin
+//					next_state <= DATA; 
+//				end
+//				DATA: begin
+//					
+//				end
+//		end
+//	end
+//
+//	always @(posedge HCLK) begin
+//		state <= next_state;
+//		case(state)
+//			IDLE: begin
+//				//idk what to do here lmao
+//			end
+//			ADDRESS: begin
+//				
+//			end
+//			DATA: begin
+//				
+//			end
+//	end
